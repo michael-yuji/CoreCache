0721b6af7b3eafc8b51be9460532ed5a84a14b2a
yuji <hatsuneyuji@yahoo.co.jp>
2016-10-23 17:03:29 -0700

    linux support - moved File Class

--- Sources/CachedFile.swift
+++ Sources/CachedFile.swift
@@ -44,6 +44,61 @@ internal struct CachedFile: Cache {
     internal var source: DispatchSourceProtocol?
     internal var file: File
     internal var timer: Timer?
+    
+    internal final class File {
+        
+        internal var path: String
+        internal var policy: CachePolicy
+        
+        internal var lastfd: Int32
+        internal var laststat: FileStatus
+        
+        internal var updatedDate: time_t
+        
+        internal var mappedData: Data?
+        internal var swap: Data?
+        
+        internal init(path: String, policy: CachePolicy, fd: Int32, stat: FileStatus, updated: time_t) {
+            self.path = path
+            self.policy = policy
+            self.lastfd = fd
+            self.laststat = stat
+            self.updatedDate = updated
+        }
+        
+        internal func update() throws {
+            if let stat = try? FileStatus(fd: self.lastfd) {
+                if stat == self.laststat {
+                    return
+                }
+            }
+            
+            let newfd = open(path, O_RDWR)
+            
+            if newfd == -1 {
+                throw CachedFile.Error.open(String.lastErrnoString)
+            }
+            
+            close(self.lastfd)
+            
+            self.lastfd = newfd
+            self.laststat = try FileStatus(fd: lastfd)
+            
+            if case .noReserve = policy {} else {
+                
+                let ptr = mmap(nil, laststat.size, PROT_READ | PROT_WRITE | PROT_EXEC , MAP_FILE | MAP_PRIVATE, lastfd, 0)
+                
+                if ptr?.numerialValue == -1 {
+                    perror("mmap")
+                } else {
+                    mappedData = Data(bytesNoCopy: UnsafeMutableRawPointer(ptr)!, count: laststat.size, deallocator: .unmap)
+                }
+            }
+            
+            self.updatedDate = time(nil)
+        }
+        
+    }
 }
 
 extension CachedFile {
